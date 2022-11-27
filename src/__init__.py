febbf1a96474e13ef252e3a228fe0c3c8eccf855
Robertales <alessio.robertazzi@gmail.com>
2022-11-27 18:55:12 +0100

    switching local db to Azure DB Services

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,54 @@
+#!/usr/bin/env python
+import os
+import urllib.parse
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_ckeditor import CKEditor
+from flask_login import LoginManager
+from sqlalchemy_utils import database_exists, create_database
 
+
+# Configure Database URI:
+params = urllib.parse.quote_plus('Driver={ODBC Driver 18 for SQL Server};Server=tcp:server-sql-qml.database.windows.net,1433;Database=quantumDB;Uid=rootQML;Pwd=R00t*quantum;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
+
+
+# initialization
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'jshwifhjwieoajhf5847f5ae4eaws'
+app.config['SQLALCHEMY_DATABASE_URI'] = "mssql+pyodbc:///?odbc_connect=%s" % params
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['CKEDITOR_FILE_UPLOADER'] = 'upload'
+db = SQLAlchemy(app)
+ckeditor = CKEditor(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "loginPage"
+login_manager.login_message_category = "info"
+
+from src.source.model import models
+
+# extensions
+db = SQLAlchemy(app)
+
+from src.source.classificazioneDataset import ClassifyControl
+from src.source.preprocessingDataset import PreprocessingControl
+from src.source.validazioneDataset import ValidazioneControl
+from src.source.gestione import GestioneControl
+from src.source.utente import UtenteControl
+from src import routes
+
+
+
+
+
+
+
+"""""
 from flask import Flask
 from flask_ckeditor import CKEditor
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_utils import database_exists, create_database
+
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:R00t*quantum@0.0.0.0/quantumknn_db"
@@ -34,3 +79,4 @@ from src.source.validazioneDataset import ValidazioneControl
 from src.source.gestione import GestioneControl
 from src.source.utente import UtenteControl
 from src import routes
+"""""
