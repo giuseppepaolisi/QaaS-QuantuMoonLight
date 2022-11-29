500339b6faabad13e2d1bf9124e3efb2614805a9
Robertales <alessio.robertazzi@gmail.com>
2022-11-29 10:16:36 +0100

    switching local db to Azure DB Services (retrying2)

--- src/__init__.py
+++ src/__init__.py
@@ -29,7 +29,7 @@ from src.source.model import models
 # Create database if it does not exist
 #if not database_exists(app.config["SQLALCHEMY_DATABASE_URI"]):
 #    create_database(app.config["SQLALCHEMY_DATABASE_URI"])
-#    with app.app_context():
+#    with app.app_context(): ciao
 #        db.create_all()
 #else:
 #    with app.app_context():
